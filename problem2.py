39155d0c5071b82067bf7607df2c726b87467a4f
Hotboy2003 <56082966+Hotboy2003@users.noreply.github.com>
2019-10-22 17:33:24 +0200

    Add files via upload
    Лабораторная номер 3

---
+++ problem2.py
@@ -0,0 +1,15 @@
+import my_module
+import math
+from math import pi
+from math import tan
+from math import cos
+h=100
+b=30
+a=pi/3
+u=((g*h*c*tan(b)**2)/((2*cos(a)**2)*(1-tan(b)*tan(a))))**0.5
+print(u)
+
+T=200
+ef=300
+N=(2/pi**0.5)*(h/(k*T)**1.5)*e*ef
+print(N)
